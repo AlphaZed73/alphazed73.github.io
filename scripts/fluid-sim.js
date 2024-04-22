c1648810e62ede34be041ffffb6107df32493960
AlphaZed73 <benjamin.woolstenhulme@gmail.com>
2024-04-22 14:12:13 -0600

    progress on fluid sim

--- scripts/fluid-sim.js
+++ scripts/fluid-sim.js
@@ -1,33 +1,32 @@
 // https://cg.informatik.uni-freiburg.de/intern/seminar/gridFluids_fluid-EulerParticle.pdf
 // board variables
 let board;
+let distance;
+let numParticles = 100;
 let boardWidth = 1280;
 let boardHeight = 720;
 let context;
 
+let t = 0;
+let dt = 0.01;
+let smoothingRadius = 0.45;
+const particles = [];
+const neighbors = [];
 
-let smoothing = 1
 function kernel(distance) {
-    return (1/(Math.sqrt(Math.PI ** 3) * (smoothing ** 3))) * (Math.E ** ((distance ** 2) / (smoothing ** 2)));
+    return Math.max(0, ((smoothingRadius ** 2) - (distance ** 2)) ** 3) / (Math.PI * (smoothingRadius ** 8) / 4);
 }
 
 class Particle {
-    constructor() {
+    constructor(position) {
         this.mass = 1;
-        this.position;
-        this.velocity;
+        this.position = position;
+        this.velocity = [0, 0];
         this.density;
         this.pressure;
         this.force;
     }
 }
-
-class Car {
-    constructor(name, year) {
-      this.name = name;
-      this.year = year;
-    }
-  }
 
 window.onload = function() {
     board = document.getElementById("board");
@@ -35,18 +34,41 @@ window.onload = function() {
     board.width = boardWidth;
     context = board.getContext("2d") // used for drawing on the board
 
+    for (let i = 0; i < numParticles; i++) {
+        particles[i] = new Particle([Math.random() * boardWidth, Math.random() * boardHeight]);
+    }
+
     requestAnimationFrame(update);
+}
+
+// main loop
+function update() {
+    requestAnimationFrame(update);
+
+    // update particles and do calculations for forces and such
+    for (let j = 0; j < numParticles; j++) {
+        neighbors = particles;
+        neighbors.pop(j);
+        
+        // evaluate each neighbor's effects
+        for (let n = 0; n < numParticles - 1; n++) {
+            // calculate distance
+            distance = Math.sqrt((particles[j].position[0] - neighbors[n].position[0]) ** 2 + (particles[j].position[1] - neighbors[n].position[1]) ** 2)
+
+            // density update
+            particles[j].density += kernel(distance) * particles[j].mass;
+        }
+    }
 }
 /*
 Set t = 0
 Choose a ∆t
 for i from 0 to n
-for j from 1 to numparticles
-Get list Lj of neighbors for Pj
-Calculate Densityj for Pj using Lj
-Calculate P ressurej for Pj using Lj
-Calculate acceleration Aj for Pj using Densityj and
-P ressurej
-Move Pj using Aj and ∆t using Euler step
-t = t + ∆t
+    for j from 1 to numparticles
+        Get list Lj of neighbors for Pj
+        Calculate Densityj for Pj using Lj
+        Calculate Pressurej for Pj using Lj
+        Calculate acceleration Aj for Pj using Densityj and Pressurej
+        Move Pj using Aj and ∆t using Euler step
+    t = t + ∆t
 */
